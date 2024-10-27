1fc10fa1d86b0a38314e254fbc774a6d1b10fce3
nmuud <nmuud@naver.com>
2024-10-27 14:53:00 +0900

    ;

---
+++ src/programmers/StackQ_1_같은숫자는싫어.java
@@ -0,0 +1,34 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class StackQ_1_같은숫자는싫어 {
+    public int[] solution(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int x : arr) {
+            // 스택이 비어있거나 스택의 최상단 값과 다른 경우에 추가
+            if (stack.isEmpty() || stack.peek() != x) {
+                stack.push(x);
+            }
+        }
+
+        // Stack 의 값을 배열로 변환하여 반환
+        int[] answer = new int[stack.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = stack.get(i);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        StackQ_1_같은숫자는싫어 T = new StackQ_1_같은숫자는싫어();
+        int[] arr1 = {1, 1, 3, 3, 0, 1, 1};
+        int[] arr2 = {4, 4, 4, 3, 3};
+
+        System.out.println(Arrays.toString(T.solution(arr1)));
+        System.out.println(Arrays.toString(T.solution(arr2)));
+    }
+}
---
+++ src/programmers/StackQ_2_기능개발.java
@@ -0,0 +1,51 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StackQ_2_기능개발 {
+    public int[] solution(int[] progresses, int[] speeds) {
+
+        int n = progresses.length;
+        int[] days = new int[n];
+        List<Integer> answer = new ArrayList<>(); // 배포되는 기능 수 저장
+
+        // 각 작업의 완료까지 걸리는 날짜 계산
+        for (int i = 0; i < n; i++) {
+            int remaining = 100 - progresses[i]; // 남은 진도
+            // 완료까지 걸리는 일수 계산 (올림 처리)
+            days[i] = (remaining + speeds[i] - 1) / speeds[i];
+        }
+
+        int cnt = 1; // 배포될 기능 수 카운트
+        int maxDay = days[0]; // 첫 번째 작업의 완료 날짜로 초기화
+
+        // 기능이 배포되는 개수 계산
+        for (int i = 1; i < n; i++) {
+            if (days[i] <= maxDay) {
+                cnt++;
+            } else {
+                answer.add(cnt);
+                cnt = 1;
+                maxDay = days[i];
+            }
+        }
+
+        answer.add(cnt);
+
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+
+    public static void main(String[] args) {
+        StackQ_2_기능개발 T = new StackQ_2_기능개발();
+        int[] progresses1 = {93, 30, 55};
+        int[] speeds1 = {1, 30, 5};
+
+        int[] progresses2 = {95, 90, 99, 99, 80, 99};
+        int[] speeds2 = {1, 1, 1, 1, 1, 1};
+
+        System.out.println(Arrays.toString(T.solution(progresses1, speeds1))); // 출력: [2, 1]
+        System.out.println(Arrays.toString(T.solution(progresses2, speeds2))); // 출력: [1, 3, 2]
+    }
+}

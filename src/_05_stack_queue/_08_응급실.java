b9f1ff8479998b7b5555fc9c5bc30a881020f325
nmuud <nmuud@naver.com>
2024-11-04 23:10:44 +0900

    ;

---
+++ src/_05_stack_queue/_08_응급실.java
@@ -0,0 +1,57 @@
+package _05_stack_queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Person {
+    int id;
+    int priority;
+
+    public Person(int id, int priority) {
+        this.id = id;
+        this.priority = priority;
+    }
+}
+
+public class _08_응급실 {
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        Queue<Person> Q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            Q.offer(new Person(i, arr[i]));
+        }
+
+        while (!Q.isEmpty()) {
+            Person tmp = Q.poll();
+            for (Person x : Q) {
+                if (x.priority > tmp.priority) {
+                    Q.offer(tmp);
+                    tmp = null;
+                    break;
+                }
+            }
+            if (tmp != null) {
+                answer++;
+                if (tmp.id == m) {
+                    return answer;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        _08_응급실 T = new _08_응급실();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(T.solution(n, m, arr));
+    }
+}

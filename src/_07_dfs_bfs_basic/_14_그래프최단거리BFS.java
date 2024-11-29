9e85a582d216d2838700df0aa5ce5d9576bcf8de
nmuud <nmuud@naver.com>
2024-11-29 19:57:26 +0900

    ;ㅁ;

---
+++ src/_07_dfs_bfs_basic/_14_그래프최단거리BFS.java
@@ -0,0 +1,52 @@
+package _07_dfs_bfs_basic;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class _14_그래프최단거리BFS {
+    static int n, m;
+    static ArrayList<ArrayList<Integer>> graph;
+    static int[] ch, dis;
+
+    public void BFS(int v) {
+        Queue<Integer> queue = new LinkedList<>();
+        ch[v] = 1;
+        dis[v] = 0;
+        queue.offer(v);
+        while (!queue.isEmpty()) {
+            int cv = queue.poll();
+            for (int nv : graph.get(cv)) {
+                if (ch[nv] == 0) {
+                    ch[nv] = 1;
+                    queue.offer(nv);
+                    dis[nv] = dis[cv] + 1;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        _14_그래프최단거리BFS T = new _14_그래프최단거리BFS();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        graph = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+        ch = new int[n + 1];
+        dis = new int[n + 1];
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph.get(a).add(b);
+        }
+        T.BFS(1);
+        for (int i = 2; i <= n; i++) {
+            System.out.println(i + " : " + dis[i]);
+        }
+    }
+}
